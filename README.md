3c4479e4db618c03f58e19fb9eeeea6c0ae5c570
Sam Alws <sam.alws@gmail.com>
2020-01-30 13:42:18 -0600

    auto page putting stuff in

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 HOW ITLL WORK:
 - single page that loads other stuff on startup
 - URL = samalws.com?[page]/[optional extra data]
-- js script will pull page from samalws.com/[page].html
+- js script will pull page from pages/[page].html
 - optional extra data: if i do a blog or something, [page].html will have a link to the post in it
---
+++ main.js
@@ -0,0 +1,12 @@
+urlParts = document.URL.split("?", 2)
+urlToGet = "loremIpsum"
+if (urlParts.length == 2) {
+	urlToGet = urlParts[1]	
+}
+requ = new XMLHttpRequest()
+requ.onload = () => {
+	alert(requ.response)
+	document.getElementById("mainSection").innerHTML = requ.response
+}
+requ.open("GET", "https://samalws.com/pages/" + urlToGet + ".html")
+requ.send()

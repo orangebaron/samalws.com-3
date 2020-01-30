f32a0a2a47e60871a60d741e203c606d7e2f2217
Sam Alws <sam.alws@gmail.com>
2020-01-30 16:58:23 -0600

    repeat until showdown exists

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,9 +1,15 @@
 defaultBlogPage = "My_Blog"
 blogRequ = (page) => {
 	xmlRequ("content/" + page + ".md", (requ) => {
-		if (requ.status == 200)
-			document.getElementById("blogContent").innerHTML = new showdown.Converter().makeHtml(requ.response)
-		else
+		if (requ.status == 200) {
+			while (true) {
+				try {
+					document.getElementById("blogContent").innerHTML = new showdown.Converter().makeHtml(requ.response)
+					intentionally error here
+					break
+				} catch () {}
+			}
+		} else
 			blogRequ(defaultBlogPage)
 	})
 }

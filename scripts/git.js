a4ca79ea0fee29e1df548e56ebbbc05e0778cf7c
Sam Alws <sam.alws@gmail.com>
2020-02-07 11:37:55 -0600

    dir:

--- scripts/git.js
+++ scripts/git.js
@@ -4,7 +4,7 @@ waitForMultiple(["fsLoaded", "git"], () => {
 
 git.plugins.set('fs', window.fs)
 
-window.gitClone = (inp) => git.clone({ inp[0], url: inp[1] })
+window.gitClone = (inp) => git.clone({ dir: inp[0], url: inp[1] })
 
 window.gitLoaded = true
 

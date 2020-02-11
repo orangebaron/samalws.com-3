74fe3add936ba9f0270948fef6ddf64d86472a89
Sam Alws <sam.alws@gmail.com>
2020-02-10 22:50:07 -0600

    addProxy

--- scripts/git.js
+++ scripts/git.js
@@ -6,7 +6,7 @@ git.plugins.set('fs', window.fs)
 
 addProxy = (obj) => Object.assign({}, obj, {corsProxy: "https://cors.isomorphic-git.org"})
 
-window.gitClone = (arg) => git.clone(fixUrl(arg))
+window.gitClone = (arg) => git.clone(addProxy(arg))
 
 window.gitLoaded = true
 

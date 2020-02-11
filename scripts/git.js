161a0fbaed28174ccd42b96d6f78b0cd89fcb0aa
Sam Alws <sam.alws@gmail.com>
2020-02-10 23:55:08 -0600

    pgp

--- scripts/git.js
+++ scripts/git.js
@@ -1,8 +1,9 @@
-// depends on: scripts/fs.js and https://unpkg.com/isomorphic-git
+// depends on: scripts/fs.js, https://@isomorphic-git/pgp-plugin, and https://unpkg.com/isomorphic-git
 
-waitForMultiple(["fsLoaded", "git"], () => {
+waitForMultiple(["fsLoaded", "git", "pgp"], () => {
 
-git.plugins.set('fs', window.fs)
+git.plugins.set("fs", window.fs)
+git.plugins.set("pgp", window.pgp) //TODO: window. necessary?
 
 addProxy = (obj) => Object.assign({}, obj, {corsProxy: "https://cors.isomorphic-git.org"})
 

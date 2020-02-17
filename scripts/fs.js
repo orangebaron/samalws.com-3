a056138dd213130f132b94736cecbb96468f04dc
Sam Alws <sam.alws@gmail.com>
2020-02-17 10:41:50 -0600

    put / between dir and arg

--- scripts/fs.js
+++ scripts/fs.js
@@ -5,13 +5,12 @@ waitFor("LightningFS", () => {
 window.fs = new LightningFS('fs')
 window.pfs = window.fs.promises
 
-processFsArg = arg => (window.dir || "") + (arg || "")
+processFsArg = arg => (window.dir || "") + "/" + (arg || "")
 
 window.cd = (_, arg) => { window.dir = "/" + arg }
 window.cdPlus = (_, arg) => { window.dir += "/" + arg }
 window.ls = (otp, arg) => pfs.readdir(processFsArg(arg)).then(otp)
 window.rm = (otp, arg) => pfs.unlink(processFsArg(arg)).then(otp)
-window.rmDir = (otp, arg) => pfs.rmdir(processFsArg(arg)).then(otp)
 
 window.fsLoaded = true
 

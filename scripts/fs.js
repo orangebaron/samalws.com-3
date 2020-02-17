be6ff55b7a8cd748ba35e8c74bf6606573675f50
Sam Alws <sam.alws@gmail.com>
2020-02-17 10:58:54 -0600

    env.dir not window.dir

--- scripts/fs.js
+++ scripts/fs.js
@@ -5,11 +5,11 @@ waitFor("LightningFS", () => {
 window.fs = new LightningFS('fs')
 window.pfs = window.fs.promises
 
-processFsArg = arg => (window.dir || "") + "/" + (arg || "")
+processFsArg = env => (env.dir || "") + "/" + (env.arg || "")
 
-window.cd = (_, arg) => { window.dir = (arg[0] != "/" && window.dir ? window.dir : "") + "/" + arg }
-window.ls = (otp, arg) => pfs.readdir(processFsArg(arg)).then(otp)
-window.rm = (otp, arg) => pfs.unlink(processFsArg(arg)).then(otp)
+window.cd = (_, __, env) => Object.assign({}, env, {dir: (env.arg[0] != "/" && env.dir ? env.dir : "") + "/" + env.arg })
+window.ls = (otp, env) => pfs.readdir(processFsArg(env)).then(otp)
+window.rm = (otp, env) => pfs.unlink(processFsArg(env)).then(otp)
 
 window.fsLoaded = true
 
--- scripts/git.js
+++ scripts/git.js
@@ -5,7 +5,7 @@ waitForMultiple(["fsLoaded", "git", "pgp"], () => {
 git.plugins.set("fs", window.fs)
 git.plugins.set("pgp", window.pgp) //TODO: window. necessary?
 
-fixGitArgs = (env, arg) => Object.assign({}, arg, {corsProxy: "https://cors.isomorphic-git.org", dir: window.dir, signingKey: env.gitKey})
+fixGitArgs = (env, arg) => Object.assign({}, arg, {corsProxy: "https://cors.isomorphic-git.org", dir: env.dir, signingKey: env.gitKey})
 
 window.gitFunc = (otp, env) => git[env.arg[0]](fixGitArgs(env, env.arg[1])).then(x => otp(JSON.stringify(x) || "done"))
 

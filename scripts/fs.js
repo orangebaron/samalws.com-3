cb427c7816cb3f2f5055d337827fb5f0d9680fab
Sam Alws <sam.alws@gmail.com>
2020-02-19 10:53:48 -0600

    I think window is unnecessary

--- scripts/fs.js
+++ scripts/fs.js
@@ -8,12 +8,12 @@ window.pfs = window.fs.promises
 processFsArg = env => (env.dir || "") + "/" + (env.arg || "")
 processFsArgNum = (env, argNum) => (env.dir || "") + "/" + (env.arg[argNum] || "")
 
-window.cd = (_, __, env) => Object.assign({}, env, {dir: (env.arg[0] != "/" && env.dir ? env.dir : "") + "/" + env.arg })
-window.ls = (otp, env) => pfs.readdir(processFsArg(env)).then(otp)
-window.rm = (otp, env) => pfs.unlink(processFsArg(env)).then(otp)
-window.mv = (otp, env) => pfs.rename(processFsArgNum(env,0), processFsArg(env,1)).then(otp)
-winow.write = (otp, env) => pfs.writeFile(processFsArgNum(env,0), env.arg[1], "utf8").then(otp)
-window.cat = (otp, env) => pfs.readFile(processFsArg(env), "utf8").then(otp)
+cd = (_, __, env) => Object.assign({}, env, {dir: (env.arg[0] != "/" && env.dir ? env.dir : "") + "/" + env.arg })
+ls = (otp, env) => pfs.readdir(processFsArg(env)).then(otp)
+rm = (otp, env) => pfs.unlink(processFsArg(env)).then(otp)
+mv = (otp, env) => pfs.rename(processFsArgNum(env,0), processFsArg(env,1)).then(otp)
+write = (otp, env) => pfs.writeFile(processFsArgNum(env,0), env.arg[1], "utf8").then(otp)
+cat = (otp, env) => pfs.readFile(processFsArg(env), "utf8").then(otp)
 
 window.fsLoaded = true
 

addb019d6d314bf1f63d6cc5c67517923694a679
Sam Alws <sam.alws@gmail.com>
2020-02-19 10:57:18 -0600

    Num

--- scripts/fs.js
+++ scripts/fs.js
@@ -11,7 +11,7 @@ processFsArgNum = (env, argNum) => (env.dir || "") + "/" + (env.arg[argNum] || "
 cd = (_, __, env) => Object.assign({}, env, {dir: (env.arg[0] != "/" && env.dir ? env.dir : "") + "/" + env.arg })
 ls = (otp, env) => pfs.readdir(processFsArg(env)).then(otp)
 rm = (otp, env) => pfs.unlink(processFsArg(env)).then(otp)
-mv = (otp, env) => pfs.rename(processFsArgNum(env,0), processFsArg(env,1)).then(otp)
+mv = (otp, env) => pfs.rename(processFsArgNum(env,0), processFsArgNum(env,1)).then(otp)
 write = (otp, env) => pfs.writeFile(processFsArgNum(env,0), env.arg[1], "utf8").then(otp)
 cat = (otp, env) => pfs.readFile(processFsArg(env), "utf8").then(otp)
 

f308d4326b3307814aefad43ef804719ec1de098
Sam Alws <sam.alws@gmail.com>
2020-02-22 01:46:50 -0600

    should actually run this time

--- scripts/pgp.js
+++ scripts/pgp.js
@@ -12,12 +12,12 @@ pgpSign = msg => openpgp.key.readArmored(msg.secretKey).then(pk => pk.keys[0].de
 
 filterValidInvalid = sigs => ({ valid: sigs.filter(x => x.valid), invalid: sigs.filter(x => !x.valid) })
 
-pgpVerify = msg => openpgp.signature.readArmored(msg.signature).then(sig =>
+pgpVerify = msg => openpgp.signature.readArmored(msg.signature).then(sig => openpgp.key.readArmored(msg.publicKey).then(pk =>
 	openpgp.verify({
 		message: openpgp.message.fromBinary(openpgp.util.encode_utf8(msg.payload)),
 		signature: sig,
-		publicKeys: (await openpgp.key.readArmored(msg.publicKey)).keys
-	}).then(filterValidInvalid))
+		publicKeys: pk.keys
+	}).then(filterValidInvalid)))
 
 pgpLoaded = true
 

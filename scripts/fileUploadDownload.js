e48bc19cd782a49064d1228d72bf7c3784a34e84
Sam Alws <sam.alws@gmail.com>
2020-02-26 22:43:26 -0600

    new blob

--- scripts/fileUploadDownload.js
+++ scripts/fileUploadDownload.js
@@ -5,6 +5,6 @@ waitFor("myPageLoaded", () => {
 fileReader = new FileReader()
 fileUpload = () => fileReader.readAsText(document.getElementById("fileUpload").files[0])
 funcs.computerToVar = noIO(env => Object.assign({}, env, { [env.arg]: fileReader.result }))
-funcs.varToComputer = notChangeEnv(noInp((otp, env) => otp("<a href='" + URL.createObjectURL(env[env.arg]) + "'>ok</a>")))
+funcs.varToComputer = notChangeEnv(noInp((otp, env) => otp("<a href='" + URL.createObjectURL(new Blob([env[env.arg]])) + "'>ok</a>")))
 
 })

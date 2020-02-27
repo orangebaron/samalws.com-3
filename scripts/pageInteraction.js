d5337f9f403556b9fb31ff87af74058e75bb8792
Sam Alws <sam.alws@gmail.com>
2020-02-26 22:55:12 -0600

    password to var

--- scripts/pageInteraction.js
+++ scripts/pageInteraction.js
@@ -6,5 +6,6 @@ fileReader = new FileReader()
 fileUpload = () => fileReader.readAsText(document.getElementById("fileUpload").files[0])
 funcs.computerToVar = noIO(env => Object.assign({}, env, { [env.arg]: fileReader.result }))
 funcs.varToComputer = notChangeEnv(noInp((otp, env) => otp("<a target='_blank' href='" + URL.createObjectURL(new Blob([env[env.arg]])) + "'>ok</a>")))
+funcs.passwordToVar = noIO(env => Object.assign({}, env, { [env.arg]: document.getElementById("password").value }))
 
 })

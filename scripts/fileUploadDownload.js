2b81d09b9001caf2f967efb9e8d88098c659fcbd
Sam Alws <sam.alws@gmail.com>
2020-02-26 22:34:05 -0600

    computerToVar

--- scripts/fileUploadDownload.js
+++ scripts/fileUploadDownload.js
@@ -4,7 +4,7 @@ waitFor("myPageLoaded", () => {
 
 fileReader = new FileReader()
 fileUpload = () => fileReader.readAsText(document.getElementById("fileUpload").files[0])
-funcs.varFromComputer = noIO(env => Object.assign({}, env, { [env.arg]: fileReader.result }))
+funcs.computerToVar = noIO(env => Object.assign({}, env, { [env.arg]: fileReader.result }))
 funcs.varToComputer = notChangeEnv(noInp((otp, env) => otp("<a href='" + Object.createObjectURL(env[env.arg]) + "'>ok</a>")))
 
 })
